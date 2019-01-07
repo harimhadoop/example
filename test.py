d5ecea2806190c0950f0256bbec4b176c96ecfca
harimhadoop <46108820+harimhadoop@users.noreply.github.com>
2019-01-08 02:36:55 +0530

    Update test.py

--- test.py
+++ test.py
@@ -91,5 +91,5 @@ df_bal3 = df_bal3.withColumn('EDUC_9514',lit(1))
 
 cols_tmp = df_bal3.columns
 cols_tmp.append('Research_pgvw360')
-cols_tmp.append('IRAs_sess360')                                                      
+cols_tmp.append('Advice_pgvw360')                                                      
 df_bal3 = df_bal3.join(uniq_poid_ret_wec, 'po_id' , how='left').select(cols_tmp)
